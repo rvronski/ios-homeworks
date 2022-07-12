0f1b93fbd28667200d8130344f61cdc70b907a39
rvronski <rvronski@gmail.com>
2022-07-12 18:05:13 +0300

    load photo

--- Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -9,4 +9,20 @@ import UIKit
 
 class ProfileView: UIView {
     
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    
+    
+    @IBOutlet weak var discriptionTextView: UITextView!
+    
 }
