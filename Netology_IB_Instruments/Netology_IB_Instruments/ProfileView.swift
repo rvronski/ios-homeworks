85489be9a7d51e882e6dbf460fa230e4affad2f4
rvronski <rvronski@gmail.com>
2022-07-12 18:12:43 +0300

    create ViewController

--- Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -25,4 +25,23 @@ class ProfileView: UIView {
     
     @IBOutlet weak var discriptionTextView: UITextView!
     
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setup()
+    }
+    private func setup(){
+        let view = self.loadViewFromXib()
+        self.addSubview(view)
+        view.frame = self.bounds
+        }
+    
+    private func loadViewFromXib() -> UIView {
+        guard let view = Bundle.main.loadNibNamed("ProfileView", owner: self)? .first as? UIView else{
+            return UIView()
+        }
+        return view
+    }
+    
+    
 }
